54f142975f7ee0c3a839ae496374416a9dcf93a3
“sandip5” <singhsandeep.singh97@gmail.com>
2020-08-16 16:22:05 +0530

    ADDED- Code To Calculate Employee Monthly Wage

--- EmpWage.cpp
+++ EmpWage.cpp
@@ -8,13 +8,17 @@ int main()
 	const int IS_FULL_TIME = 1;
 	const int IS_PART_TIME = 2;
 	const int EMP_RATE_PER_HR = 20;
-	int empHrs;
+	const int MONTH_TOTAL_WORKING_DAYS = 20;
+	int empHrs = 0;
 	int dailyWage = 0;
+	int monthWage = 0;
 
 	cout << "Welcome To Employee Wage Computation Problem" << endl;
 	srand( time(0) );
-	int checkAttendance = rand() % 3 + 1;
 
+	for ( int day = 1; day <= MONTH_TOTAL_WORKING_DAYS; day++ )
+	{
+	int checkAttendance = rand() % 3 + 1;
 	if( IS_FULL_TIME == checkAttendance )
 	{
 		empHrs = 8;
@@ -27,8 +31,10 @@ int main()
 	{
 		empHrs = 0;
 	}
+	dailyWage = EMP_RATE_PER_HR * empHrs;
+	monthWage = monthWage + dailyWage;
+	}
 
-	dailyWage = EMP_RATE_PER_HR * empHrs; 
-	cout << "Employee Daily Wage: " << dailyWage << endl;
+	cout << "Employee Monthly Wage: " << monthWage << endl;
 	return 0;
 }
